1a53c769f3223f144077cf2c39e83e79c4546b6a
Craig Munro <soberproductions@gilmer.dreamhost.com>
2013-12-23 09:22:14 -0800

    Compat

--- 2013/index.php
+++ 2013/index.php
@@ -29,12 +29,9 @@ error_reporting(E_ALL);
 <!--[if lt IE 9]>
 <script>
 document.createElement('header');
-document.createElement('nav');
+document.createElement('main');
 document.createElement('section');
-document.createElement('article');
-document.createElement('aside');
 document.createElement('footer');
-document.createElement('hgroup');
 </script>
 <![endif]-->
 </head>
